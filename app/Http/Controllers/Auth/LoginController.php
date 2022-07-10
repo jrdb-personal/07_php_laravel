7de2d29b78714c9aeaa95d89b8b966c20da5e0c5
jbanez <jamesronaldbanez@gmail.com>
2022-07-11 07:55:44 +0800

    change email to username on auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,18 +21,8 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
@@ -40,7 +30,7 @@ class LoginController extends Controller
 
     public function username()
     {
-	    return 'name';
+	    return 'email';
     }
 
 }
